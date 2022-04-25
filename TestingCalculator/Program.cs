e9159e407331841860d68833eaf2b01b2f9e8f5a
h4irullin.adel@yandex.ru <h4irullin.adel@yandex.ru>
2022-04-25 21:29:52 +0300

    Add project files.

---
+++ TestingCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your counting string");
+                try
+                {
+                    Console.WriteLine(Calculator.Calculate(Console.ReadLine()));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input data is incorrect \n Please, try again ");
+                }
+            }
+        }
+    }
+}
